57def2f1690ebb444cc57f708c4f131465f99389
Hung Tran <hungtran30112004@gmail.com>
2025-03-14 22:41:01 +0700

    Merge commit 'fc75f27'

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { CartControllers } from "../controllers/cart.controller";
+import { userMiddleware } from "../middleware/user.middleware";
 
 const router = Router()
 
 router.route("/cart")
+  .get(userMiddleware, CartControllers.getAllUserCart)
+  .post(userMiddleware, CartControllers.addToCart)
+  .delete(userMiddleware, CartControllers.deleteCartItem)
