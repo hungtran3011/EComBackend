39bdaa1f8c3c98ccdb964e84f3c9e70d1c74fd6f
Hung Tran <hungtran30112004@gmail.com>
2025-03-18 21:44:04 +0700

    [FIX] add rate limiters for auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,10 @@
+import { IPRateLimiter } from "../config/rate-limit"
 import AuthControllers from "../controllers/auth.controller"
 
-router.get("/sign-in", AuthControllers.signIn)
+router.get("/sign-in",IPRateLimiter, AuthControllers.signIn)
 
-router.post("/sign-up", AuthControllers.registerUser)
+router.post("/sign-up", IPRateLimiter, AuthControllers.registerUser)
+
+router.post("/sign-out", IPRateLimiter, AuthControllers.handleLogout)
+
+router.post("/refresh-token", IPRateLimiter, AuthControllers.handleRefreshToken)
