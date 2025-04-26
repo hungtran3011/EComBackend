49b420bd5db233d119f7d608d854dfa85388341b
hungtran3011 <hungtran30112004@gmail.com>
2025-04-26 09:57:00 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ if (!redisService.isConnected()) {
 
 
 securityMiddleware(app);
-app.set("trust proxy", 1); // trust first proxy
+app.set("trust proxy", true); // trust first proxy
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
